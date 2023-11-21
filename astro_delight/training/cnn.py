3fdb1a2a906c7953f4fcde90e962017b342f9c6f
Keviinplz <kevin.pinochet@ug.uchile.cl>
2023-11-21 17:06:07 -0300

    fix (training/cnn): Changed signature to  simplifly train function

--- astro_delight/training/cnn.py
+++ astro_delight/training/cnn.py
@@ -7,10 +7,7 @@ from ray import train
 from ray.train import Checkpoint
 from torch.utils.data import DataLoader
 
-from astro_delight.models.cnn.model import (
-    DelightCnn,
-    DelightCnnParameters,
-)
+from astro_delight.models.cnn.model import DelightCnn, DelightCnnParameters
 from astro_delight.training.dataset import (
     DelightDataset,
     DelightDatasetOptions,
@@ -18,26 +15,46 @@ from astro_delight.training.dataset import (
 )
 
 
-class HyperParametersOptions(TypedDict):
+class HyperParameters(TypedDict):
     lr: float
     batch_size: int | float
-    epochs: int
+    nconv1: int | float
+    nconv2: int | float
+    nconv3: int | float
+    ndense: int | float
+    dropout: float
 
 
-class TrainDelightCnnModelParameters(TypedDict):
-    dataset: DelightDatasetOptions
-    model: DelightCnnParameters
-    hyperparams: HyperParametersOptions
-    device: str
+def _get_value_from_parameter(parameter: int | float, base: int = 2) -> int:
+    return int(base**parameter) if isinstance(parameter, float) else parameter
 
 
-def train_delight_cnn_model(params: TrainDelightCnnModelParameters):
-    net = DelightCnn(params["model"])
+def get_delight_cnn_parameters(
+    params: HyperParameters, options: DelightDatasetOptions
+) -> DelightCnnParameters:
+    return {
+        "nconv1": _get_value_from_parameter(params["nconv1"]),
+        "nconv2": _get_value_from_parameter(params["nconv2"]),
+        "nconv3": _get_value_from_parameter(params["nconv3"]),
+        "ndense": _get_value_from_parameter(params["ndense"]),
+        "levels": options.n_levels,
+        "dropout": _get_value_from_parameter(params["dropout"]),
+        "rot": options.rot,
+        "flip": options.flip,
+    }
+
+
+def train_delight_cnn_model(params: HyperParameters, options: DelightDatasetOptions):
+    batch_size = _get_value_from_parameter(params["batch_size"])
+    lr = _get_value_from_parameter(params["lr"], base=10)
+
+    device = "cpu" if torch.cuda.is_available() is False else "cuda"
+    net = DelightCnn(get_delight_cnn_parameters(params, options))
     net.train(True)
-    net.to(params["device"])
+    net.to(device)
 
     criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=10 ** params["hyperparams"]["lr"])
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     checkpoint = cast(Checkpoint | None, train.get_checkpoint())  # type: ignore
     start_epoch = 0
 
@@ -48,28 +65,20 @@ def train_delight_cnn_model(params: TrainDelightCnnModelParameters):
             net.load_state_dict(checkpoint_dict["net_state_dict"])
             optimizer.load_state_dict(checkpoint_dict["optimizer_state_dict"])
 
-    train_dataset = DelightDataset(
-        options=params["dataset"], datatype=DelightDatasetType.TRAIN
-    )
+    train_dataset = DelightDataset(options=options, datatype=DelightDatasetType.TRAIN)
     val_dataset = DelightDataset(
-        options=params["dataset"], datatype=DelightDatasetType.VALIDATION
-    )
-
-    batch_size = (
-        int(2 ** params["hyperparams"]["batch_size"])
-        if isinstance(params["hyperparams"]["batch_size"], float)
-        else params["hyperparams"]["batch_size"]
+        options=options, datatype=DelightDatasetType.VALIDATION
     )
     train_dl = DataLoader(train_dataset, batch_size=batch_size)
     val_dl = DataLoader(val_dataset, batch_size=batch_size)
 
     # Training
 
-    running_loss = 0.0
-    train_loss = 0.0
-    for epoch in range(start_epoch, params["hyperparams"]["epochs"]):
+    for epoch in range(start_epoch, 50):
+        running_loss = 0.0
+        train_loss = 0.0
         for i, (inputs, labels) in enumerate(train_dl):
-            inputs, labels = inputs.to(params["device"]), labels.to(params["device"])
+            inputs, labels = inputs.to(device), labels.to(device)
 
             optimizer.zero_grad()
 
@@ -82,11 +91,8 @@ def train_delight_cnn_model(params: TrainDelightCnnModelParameters):
 
             running_loss += float(loss.item())
 
-            if (
-                i % params["hyperparams"]["batch_size"]
-                == params["hyperparams"]["batch_size"] - 1
-            ):  # print every 2000 mini-batches
-                train_loss = running_loss / params["hyperparams"]["batch_size"]
+            if i % batch_size == batch_size - 1:
+                train_loss = running_loss / batch_size
                 print(
                     "[%d, %5d] loss: %.3f"
                     % (epoch + 1, i + 1, running_loss / (epoch + 1))
@@ -99,8 +105,8 @@ def train_delight_cnn_model(params: TrainDelightCnnModelParameters):
         with torch.no_grad():
             for vinputs, vlabels in val_dl:
                 vinputs, vlabels = (
-                    vinputs.to(params["device"]),
-                    vlabels.to(params["device"]),
+                    vinputs.to(device),
+                    vlabels.to(device),
                 )
 
                 voutputs = net(vinputs)
